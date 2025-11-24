668bbca4612628e7ff4b6fc5d3790ce8e113411d
Yeison Stiven Acevedo Loaiza <yeison.acevedo2708@gmail.com>
2025-11-24 10:06:45 -0500

    Cambio de premium a lenny

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -132,7 +132,7 @@ export default function Hero() {
             `,
           }}
         >
-          Realza tu belleza con nuestra corsetería premium
+          Realza tu belleza con nuestra corsetería Lenny
         </motion.p>
       </div>
 
