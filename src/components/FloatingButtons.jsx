9171a8013bebbf86cf4d4117215fb8e1d9c47727
Yeison Stiven Acevedo Loaiza <yeison.acevedo2708@gmail.com>
2025-11-24 10:27:16 -0500

    Agregue el Instagram

--- src/components/FloatingButtons.jsx
+++ src/components/FloatingButtons.jsx
@@ -1,24 +1,35 @@
 import React from "react";
-import { FaArrowUp, FaWhatsapp, FaCog } from "react-icons/fa";
-import "./FloatingButtons.css"; // IMPORTANTE
+import { FaArrowUp, FaWhatsapp, FaInstagram } from "react-icons/fa";
+import "./FloatingButtons.css";
 
 export default function FloatingButtons() {
   return (
     <div className="floating-container">
-      <div style={{ position: "relative" }}>
-        {/* <div className="whatsapp-bubble">¿Necesitas ayuda?</div> */}
+      
 
-        <a
-          href="https://wa.me/573216131969?text=Buenas%20tardes%2C%20%C2%BFtienes%20alg%C3%BAn%20producto%3F"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <button className="floating-button whatsapp">
-            <FaWhatsapp size={28} />
-          </button>
-        </a>
-      </div>
+      {/* WhatsApp */}
+      <a
+        href="https://wa.me/573216131969?text=Buenas%2C%20%C2%BFtienes%20alg%C3%BAn%20producto%3F"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <button className="floating-button whatsapp">
+          <FaWhatsapp size={28} />
+        </button>
+      </a>
 
+      {/* Instagram */}
+      <a
+        href="https://www.instagram.com/creacioneslenny?igsh=bXk2c3h3NThhZ2t0"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <button className="floating-button instagram">
+          <FaInstagram size={26} />
+        </button>
+      </a>
+
+      {/* Flecha hacia arriba */}
       <button
         className="floating-button up"
         onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
