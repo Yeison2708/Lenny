ebecf04ae077f286784065145966a15641faa324
Yeison Stiven Acevedo Loaiza <yeison.acevedo2708@gmail.com>
2025-11-24 09:40:53 -0500

    Corrección en el titulo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,16 @@ import logo from "../assets/logos.png";
 export default function Navbar() {
   return (
     <nav className="nav">
+
       <div className="brand">
         <img src={logo} alt="logo" className="brand-logo" />
-        <h1>Confección y Creacíones Lenny</h1>
       </div>
+
+      <div className="title-box">
+        <h1 className="title-main">Confección y Creaciones</h1>
+        <h1 className="title-lenny">Lenny</h1>
+      </div>
+
     </nav>
   );
 }
